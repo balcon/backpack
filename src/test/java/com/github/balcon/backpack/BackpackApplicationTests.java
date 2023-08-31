48d796642d87923d45b8561ee4438ab07d1c3cfd
Konstantin Balykov <balcon@inbox.ru>
2023-08-31 20:15:18 +0300

    Init commit

---
+++ src/test/java/com/github/balcon/backpack/BackpackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.balcon.backpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackpackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
